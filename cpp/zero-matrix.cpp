24b98efda321903d7cd824dab7d11268a4d113db
annabush092 <annabush092@gmail.com>
2018-01-24 15:56:00 -0500

    figuring out passing matrix to and from fxns not done yet

--- cpp/zero-matrix.cpp
+++ cpp/zero-matrix.cpp
@@ -21,28 +21,36 @@
 // define matrix column size as global constant so we can pass to a function
 const int COLS = 4;
 
-
+int* makeMatrix(int);
 
 int main() {
 
   //create a matrix that has dimensions m x n, of random ints between 0 and 9
   int m = 3;
-  int n = COLS;
-  int matrix[m][n];
+  // int matrix[m][n];
+  int *mainMatrix = makeMatrix(m);
+
+  return 0;
+}
+
+int* makeMatrix(int rows) {
+
+  int *matrix = new int[rows][COLS];
+  // int matrix[rows][cols];
+
   std::cout << "Original matrix: \n[ \n";
-  for(int i=0; i<m; i++) {
-    for(int j=0; j<n; j++) {
+  for(int i=0; i<rows; i++) {
+    for(int j=0; j<COLS; j++) {
       matrix[i][j] = rand() % 10;
       std::cout << "  " << matrix[i][j];
     }
     std::cout << std::endl;
   }
   std::cout << "] \n";
-
-  return 0;
+  return matrix[0][0];
 }
 
 // remember: arrays are default passed by reference (matr[] is a pointer to the first element of the matrix). Adding const makes it so we are not allowed to modify the original array.
-void zeroMatrix(const int matr[][COLS], int rows) {
-  return;
-}
+// int* zeroMatrix(const int matr[][COLS], int rows) {
+//   return;
+// }
