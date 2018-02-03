7b132feda7789759d7ba6a39dfd5da259f565a51
annabush092 <annabush092@gmail.com>
2018-02-03 15:32:00 -0500

    beginning java working

---
+++ beginning-java/HelloWorld.java
@@ -0,0 +1,10 @@
+// To run, navigate directly into the current directory
+// javac HelloWorld.java   // to compile ?
+// java HelloWorld    // to run
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    // Prints "Hello, World" to the terminal window.
+    System.out.println("Hello, World");
+  }
+}
