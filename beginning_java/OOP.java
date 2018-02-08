9d0d8f14c51ba620df2caa1a9b0d01f5272ccee4
annabush092 <annabush092@gmail.com>
2018-02-08 14:42:37 -0500

    added comments

--- beginning_java/OOP.java
+++ beginning_java/OOP.java
@@ -4,13 +4,18 @@ package beginning_java;
 
 public class OOP {
   public static void main(String[] args) {
+
+    // test default constructor and variable getters.
+    // cannot access personInstance.name or .age directly because they are private.
     Person defaultPerson = new Person();
     System.out.println("Default name: " + defaultPerson.getName());
     System.out.println("Default age: " + defaultPerson.getAge());
 
+    // test overloaded constructor, private/public methods for updating age.
     Person anna = new Person("Anna", 26);
     System.out.println("My name: " + anna.getName());
     System.out.println("My age: " + anna.getAge());
+    // cannot use .addToAge() because it is private.
     anna.itsMyBirthday();
     System.out.println("My updated age: " + anna.getAge());
 
