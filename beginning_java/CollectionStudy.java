91dfd01efa0daf60ac42b83a4738ac201672cea2
annabush092 <annabush092@gmail.com>
2018-02-08 12:26:29 -0500

    organized code a bit

--- beginning_java/CollectionStudy.java
+++ beginning_java/CollectionStudy.java
@@ -1,25 +1,41 @@
+// Testing of general interface of different types of collections
+
 package beginning_java;
 import java.util.*;
 
 public class CollectionStudy {
   public static void main(String[] args) {
-    //Basic interface for arrays: creating, populating, and printing
+    arrayTest();
+    arrayListTest();
+    hashSetTest();
+    treeSetTest();
+    linkedHashSetTest();
+  }
+
+  //Basic interface for arrays: creating, populating, and printing
+  private static void arrayTest() {
     int[] arr = new int[5];
     arr[0] = 1;
     arr[1] = 2;
+    arr[0] = 3;
     System.out.println("Regular array first element: " + arr[0]);
     // I believe the next line returns a pointer. It certainly does not show the array contents.
     System.out.println("Regular array pointer to arr beginning: " + arr);
+  }
 
+  private static void arrayListTest() {
     //Basic interface for ArrayLists: creating, populating, and printing
     ArrayList<Integer> al = new ArrayList<Integer>(5);
     al.add(1);
     al.add(2);
     System.out.println("ArrayList first element: " + al.get(0));
     System.out.println("ArrayList all elements: " + al);
+  }
 
-    //Basic interface for HashSets: creating, populating, and printing
-    // Note, because it is unordered, we cannot use index numbers or .get. We can just read it though.
+  //Basic interface for HashSets: creating, populating, and printing
+  private static void hashSetTest() {
+    // Note, because it is unordered, we cannot use index numbers or .get.
+    // To read, we read the entire set.
     HashSet<Integer> hs = new HashSet<Integer>(5);
     hs.add(3);
     hs.add(2);
@@ -29,9 +45,11 @@ public class CollectionStudy {
     // Regardless of order we add the elements, they come out sorted.
     // Note the duplicate is not stored at all.
     System.out.println("HashSet elements: " + hs);
+  }
 
-    //Basic interface for TreeSets: creating, populating, and printing
-    // We cannot set an initial size for a TreeSet.
+  //Basic interface for TreeSets: creating, populating, and printing
+  private static void treeSetTest() {
+    // We cannot set an initial size for a TreeSet (Different argument options are available)
     TreeSet<Integer> ts = new TreeSet<Integer>();
     ts.add(3);
     ts.add(2);
@@ -41,18 +59,19 @@ public class CollectionStudy {
     // Regardless of order we add the elements, they come out sorted.
     // Note the duplicate is not stored at all.
     System.out.println("TreeSet elements: " + ts);
+  }
 
-    //Basic interface for LinkedHashSets: creating, populating, and printing
+  //Basic interface for LinkedHashSets: creating, populating, and printing
+  private static void linkedHashSetTest() {
     LinkedHashSet<Integer> lhs = new LinkedHashSet<Integer>();
     lhs.add(3);
     lhs.add(2);
     lhs.add(1);
     lhs.add(2);
     lhs.add(4);
-    // Regardless of order we add the elements, they come out sorted.
+    // Elements are stored in the order that they are added.
     // Note the duplicate is not stored at all.
-    System.out.println("LinkedHashSet elements: " + ts);
-
-
+    System.out.println("LinkedHashSet elements: " + lhs);
   }
+
 }
